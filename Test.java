f7f4ba9c5c618699194d5b774aeb6662d9211b6b
Neko <60512800+DiveshTheReal@users.noreply.github.com>
2020-10-11 15:32:48 -0400

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Hello World");
+		System.out.println("Hello World");
+		//System.out.print("Hello");
+		//System.out.println(" World");
+	}
+
+}
