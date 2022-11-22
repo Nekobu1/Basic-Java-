d029c7d277973558316610c0c585f196055c4194
Divesh <80365717+nekoism@users.noreply.github.com>
2022-11-22 16:19:06 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,8 +2,8 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-		System.out.print("Hello World");
-		System.out.println("Hello World");
+		System.out.print("Hello World"); 
+		System.out.println("Hello World"); //println just breaks and goes to next line
 		//System.out.print("Hello");
 		//System.out.println(" World");
 	}
